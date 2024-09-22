b9bc850f19447fc858a89a8d28815c406676eb9c
Xaxatyn4uk <azat2003rayan@gmail.com>
2024-09-22 13:46:07 +0300

    задание 1

--- it_park_bot/bot.py
+++ it_park_bot/bot.py
@@ -1,11 +1,21 @@
 import telebot;
 bot = telebot.TeleBot('7002682336:AAFKLNJj93CmNFepx5145-x7x6FWho93wQ0'); #тут токен бота
+name = ''
+age = 0
 @bot.message_handler(content_types=['text']) #слушаем бота
 def get_text(message):
     if message.text == "Привет": #проверям сообщение от пользователя
-        bot.send_message(message.from_user.id, "Привет Друг!") #отвечаем пользователю
+        bot.send_message(message.from_user.id, "Привет Друг! Как тебя зовут?") #отвечаем пользователю
+        bot.register_next_step_handler(message, get_name)
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "напиши: Привет")
-    else:
-        bot.send_message(message.from_user.id, "я тебя не понимаю, напиши '/help'")
+def get_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, f'{name} сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
+def get_age(message):
+    age = int(message.text)
+    bot.send_message(message.from_user.id, f'{name} вы {2024 - age} года рождения')
+
 bot.polling(none_stop=True, interval=0)# бот постоянно будет опрашивает сервер
