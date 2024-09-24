9867b5412cc18c52d8d33a09cad13b3ecc205d5e
Xaxatyn4uk <azat2003rayan@gmail.com>
2024-09-24 19:24:38 +0300

    Добавили кнопки

--- it_park_bot/bot.py
+++ it_park_bot/bot.py
@@ -1,9 +1,14 @@
-import telebot;
-bot = telebot.TeleBot('7002682336:AAFKLNJj93CmNFepx5145-x7x6FWho93wQ0'); #тут токен бота
+from telebot import types, TeleBot
+from dotenv import load_dotenv #сначала надо импортировать pip install python-dotenv
+import os
+
+load_dotenv()
+bot = TeleBot(os.getenv('TOKEN'))
+
 name = ''
 age = 0
 @bot.message_handler(content_types=['text']) #слушаем бота
-def get_text(message):
+def get_start(message):
     if message.text == "Привет": #проверям сообщение от пользователя
         bot.send_message(message.from_user.id, "Привет Друг! Как тебя зовут?") #отвечаем пользователю
         bot.register_next_step_handler(message, get_name)
@@ -15,7 +20,9 @@ def get_name(message):
     bot.send_message(message.from_user.id, f'{name} сколько тебе лет?')
     bot.register_next_step_handler(message, get_age)
 def get_age(message):
-    age = int(message.text)
-    bot.send_message(message.from_user.id, f'{name} вы {2024 - age} года рождения')
-
+    global age
+    age = int(message.text) 
+    bot.send_message(message.from_user.id, f' {name}, вы {2024 - age} года рождения')
+    bot.register_next_step_handler(message, get_start)
+        
 bot.polling(none_stop=True, interval=0)# бот постоянно будет опрашивает сервер
