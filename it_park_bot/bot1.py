9093c0dcef1ac6f61c9b6c4c1f0991827dcd33f6
Xaxatyn4uk <azat2003rayan@gmail.com>
2024-09-24 19:41:54 +0300

    Кнопки

--- it_park_bot/bot1.py
+++ it_park_bot/bot1.py
@@ -1,6 +1,7 @@
 from telebot import types, TeleBot
 from dotenv import load_dotenv #сначала надо импортировать pip install python-dotenv
 import os
+
 load_dotenv()
 bot = TeleBot(os.getenv('TOKEN'))
 
